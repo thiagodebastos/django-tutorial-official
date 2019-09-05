02716e044f18398603888da1fbb5c6835517f5a7
Thiago de Bastos <thiago@thiagodebastos.com>
2019-09-06 05:25:43 +1000

    feat(voting form): implement voting
    This commit takes into account race conditions and uses
    Django's F()

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
+from django.db.models import F
+from .models import Choice, Question
 
 
 def index(request):
@@ -18,9 +20,36 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+
+    else:
+        # When Django encounters an instance of F(), it overrides the standard Python
+        # operators to create an encapsulated SQL expression
+        selected_choice.votes = F('votes') + 1
+
+        # Save the object back to the database
+        selected_choice.save()
+
+        # To access the new value saved this way, the object must be reloaded:
+        selected_choice.refresh_from_db()
+
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
